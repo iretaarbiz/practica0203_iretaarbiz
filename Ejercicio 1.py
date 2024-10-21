1c17bf5ad35982c0e46707ed921cc2bd0555d7ca
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-10-21 11:33:43 +0200

    Ejercicio 4
    Subo ejercicio 4 y corrección del 1

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -2,7 +2,7 @@
 pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida 
 por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas. '''
 
-clave = "contraseña"
+clave = input("Crea la contraseña: \n")
 contra = input("Introduce la contraseña: \n")
 if contra.lower() == clave.lower():
     print("La contraseña introducida coincide")
