f13880d1b65ed3ef2b089774131a782f79598f32
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-10-25 08:28:42 +0200

    Ejercicio 9
    Subo ejercicio 9

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -1,4 +1,13 @@
+'''Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo que tenga tantas líneas como el número introducido, como el triángulo de más abajo.
+1
+3 1
+5 3 1
+7 5 3 1
+9 7 5 3 1'''
+
 num = int(input("Introduce un número entero: \n"))
-for i in range(1, num+1):
-    for j in range(1, i, 2):
-        print(j, end="")
+for i in range(num):
+    for j in range(1, 2 * (i + 1)):
+        if j % 2 == 1: 
+            print(2 * (i + 1) - j, end="")
+    print(end="\n")
